bc13ad1065b4f17c31d806dd120d01184e6ead87
celso <celsogubeal@gmail.com>
2021-03-29 14:18:12 -0300

    Diminuido acoplamento utilizando interface

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.di.modelo.Cliente;
 
 @Component
-public class NotificadorEmail {
+public class NotificadorEmail implements Notificador {
 	public NotificadorEmail() {
 		System.out.println("NotificadorEmail");
 	}
 	
+	@Override
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando %s através de email %s: %s\n",
 				cliente.getNome(), cliente.getEmail(), mensagem );
